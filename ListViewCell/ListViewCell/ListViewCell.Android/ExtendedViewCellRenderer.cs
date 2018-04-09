1e8f97f00acacebcdecef7682287fc9b7b95eb05
MansiShah04 <38098643+MansiShah04@users.noreply.github.com>
2018-04-09 13:57:24 +0530

    Add files via upload

---
+++ ListViewCell/ListViewCell/ListViewCell.Android/ExtendedViewCellRenderer.cs
@@ -0,0 +1,54 @@
+﻿using Android.Content;
+using Android.Views;
+using ListViewCell;
+using ListViewCell.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using System.ComponentModel;
+
+[assembly: ExportRenderer(typeof(ExtendedViewCell), typeof(ExtendedViewCellRenderer))]
+
+namespace ListViewCell.Droid
+{
+    public class ExtendedViewCellRenderer : ViewCellRenderer
+    {
+        
+        private Android.Views.View _cellCore;
+        private Drawable _unselectedBackground;
+        private bool _selected;
+
+        protected override Android.Views.View GetCellCore(Cell item,
+                                                          Android.Views.View convertView,
+                                                          ViewGroup parent,
+                                                          Context context)
+        {
+            _cellCore = base.GetCellCore(item, convertView, parent, context);
+
+            _selected = false;
+            _unselectedBackground = _cellCore.Background;
+
+            return _cellCore;
+        }
+
+        protected override void OnCellPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            base.OnCellPropertyChanged(sender, args);
+
+            if (args.PropertyName == "IsSelected")
+            {
+                _selected = !_selected;
+
+                if (_selected)
+                {
+                    var extendedViewCell = sender as ExtendedViewCell;
+                    _cellCore.SetBackgroundColor(extendedViewCell.SelectedBackgroundColor.ToAndroid());
+                }
+                else
+                {
+                    _cellCore.SetBackground(_unselectedBackground);
+                }
+            }
+        }
+    }
+}
---
+++ ListViewCell/ListViewCell/ListViewCell/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewCell
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            listTemp.ItemsSource = new List<string>(new List<string> { "1","2","3","4","5"});
+        }
+
+    }
+}
